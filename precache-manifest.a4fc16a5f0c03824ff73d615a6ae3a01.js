6ced0d28d53e5c0a8e2a641eb16ce0e55ad85518
claire froelich <claire.froelich@gmail.com>
2020-09-05 18:18:30 -0700

    Updates

---
+++ precache-manifest.a4fc16a5f0c03824ff73d615a6ae3a01.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f9ebfeda0812194b623ffb84096dfa5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b68c182cb2a1ac638cde",
+    "url": "./static/css/main.88ea5c78.chunk.css"
+  },
+  {
+    "revision": "1fc3b23ab4325d0aaabe",
+    "url": "./static/js/2.6f181b61.chunk.js"
+  },
+  {
+    "revision": "e58b2fed1a83725c5b0108382043ede5",
+    "url": "./static/js/2.6f181b61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b68c182cb2a1ac638cde",
+    "url": "./static/js/main.da2786e2.chunk.js"
+  },
+  {
+    "revision": "12e1d69784e94e4c45f7",
+    "url": "./static/js/runtime-main.452bd7a2.js"
+  }
+]);
