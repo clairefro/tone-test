637d8f523425b7a22b5772cb5a849f43f91e1506
claire froelich <claire.froelich@gmail.com>
2020-09-05 18:21:17 -0700

    add background image credit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,18 @@ function App() {
   return (
     <div className="App">
       <Player />
+      <br />
+      <br />
+      <p style={{ fontWeight: "normal", fontSize: "14px" }}>
+        Background image by{" "}
+        <a
+          href="https://unsplash.com/photos/uhjiu8FjnsQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Unsplash
+        </a>
+      </p>
     </div>
   );
 }
